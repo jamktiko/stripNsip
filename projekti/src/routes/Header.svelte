b6ee7e2ffe7d3ab11dedbe76a2d112f918cd0038
Aino Seppi <ag2490@student.jamk.fi>
2025-04-28 11:56:19 +0300

    eka versio Headerista, jossa logo, otsikko ja ?-button

---
+++ projekti/src/routes/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let { otsikko, logo }: { otsikko: string; logo: string } = $props();
+</script>
+
+<header>
+  <img src={logo} alt="Strip 'n sip logo" />
+
+  <h1>{otsikko}</h1>
+
+  <button>?</button>
+</header>
+
+<style>
+  button {
+    border: 1px solid black;
+  }
+</style>
