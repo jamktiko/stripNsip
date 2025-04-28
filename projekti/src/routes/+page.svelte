b7de40455978f3b99b533072587a7ce243d9e35a
Jeremia Vepsäläinen <Jeremia.vepsalainen@gmail.com>
2025-04-28 13:13:04 +0300

    tehty esimerkkibutton ja toiminnallisuus kokeilua varten

--- projekti/src/routes/+page.svelte
+++ projekti/src/routes/+page.svelte
@@ -2,5 +2,12 @@
 	import Kortti from './Kortti.svelte';
 </script>
 
+<button
+	class=" min-h-65 max-w-50 min-w-50 text-parisuhdeteksti shadow-xl/30 m-10 grid max-h-80 rounded-xl border-8 border-white"
+	onclick={() => {
+		console.log('toimii');
+	}}>paina tätä</button
+>
+
 <style>
 </style>
