d6b85efb6ecd0f7d3a8da6dec75caf58c10718b8
Samuli, Ilomäki <ag3033@student.jamk.fi>
2025-04-30 11:21:59 +0300

    kuvat laitettu pics kansioon

--- projekti/src/routes/Header.svelte
+++ projekti/src/routes/Header.svelte
@@ -22,6 +22,7 @@
   {#snippet header()}
     <HeaderButton text="x" onclick={() => popupIkkunaNaytetaan = false} />
     <HeaderButton text="?" disabled={true} />
+    <img src="https://api.mtvuutiset.fi/graphql/caas/v1/media/5236266/data/78bad80a8e026613bc09d64c8941749c/landscape16_9/1600/korttipakka.jpg" alt="terve">
     <h1 class="text-center p-2 text-xl">Strip 'n Sip</h1>
   {/snippet}
   <p class="text-center p-2">Tämä peli sopii erilaisiin tilanteisiin, joissa halutaan viettää aikaa yhdessä rennosti ja hauskasti. Pariskunta-kategoria tarjoaa syvällisiä keskusteluja, juomapeli tuo mukaan rohkeampia kysymyksiä ja tehtäviä ystävien iltaan, ja Never have I ever -pakka toimii kevyenä tutustumisleikkinä. Pelaaminen on yksinkertaista: valitse korttipakka, napauta sitä ja vastaa esitettyyn kysymykseen tai suorita annettu tehtävä yhdessä.</p>
