6025cef47c67f81728bf42410c2a146a1c7059ca
Aino Seppi <ag2490@student.jamk.fi>
2025-05-06 10:14:32 +0300

    headerbuttonista pois turhaa koodia

--- projekti/src/routes/HeaderButton.svelte
+++ projekti/src/routes/HeaderButton.svelte
@@ -1,17 +1,10 @@
 <script lang="ts">
 	interface Props {
 		text: string;
-		onclick?: () => void;
-		disabled?: boolean;
+		onclick: () => void;
 	}
 
-	let { text, onclick, disabled = false }: Props = $props();
+	let { text, onclick }: Props = $props();
 </script>
 
-<button class="text-3xl sm:text-4xl hover:shadow-xl border-4 border-tekstit cursor-pointer hover:bg-tekstit py-1 px-4 rounded-full text-tekstit hover:text-white"{onclick} {disabled}>{text}</button>
-
-<style>
-  /* button {
-    border: 3px solid black;
-  } */
-</style>
+<button class="text-3xl sm:text-4xl hover:shadow-xl border-4 border-tekstit cursor-pointer hover:bg-tekstit py-1 px-4 rounded-full text-tekstit hover:text-white" {onclick}>{text}</button>
