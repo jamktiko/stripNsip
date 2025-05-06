2ff0f8001663dfde1fcf72dd3b2906cd64da27ef
Samuli, Ilomäki <ag3033@student.jamk.fi>
2025-05-06 11:33:22 +0300

    poistettu button dublikaatti

--- projekti/src/routes/Kortti.svelte
+++ projekti/src/routes/Kortti.svelte
@@ -19,11 +19,7 @@
 	}: Props = $props();
 </script>
 
-<button {onclick} class={className}>
-	<div class="font-josefin pt-12 text-xl lg:text-2xl pl-5 pr-5">{text}</div>
-	<span class="grid justify-items-center pr-2"
-		><img src={ikoni} class="min-w-25 max-w-25 lg:min-w-30 lg:max-w-30 2xl:min-w-35 2xl:max-w-35 " />
-	</span>
+
 <button {onclick} class="card {flippaus ? 'flipped' : ''} {className}">
 	<div class="front">
 		<div class="font-josefin pt-9 text-2xl">{text}</div>
