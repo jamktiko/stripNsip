c4ba70f30d61c617f6edeafe61fe88a676585297
Samuli, Ilomäki <ag3033@student.jamk.fi>
2025-05-07 10:45:47 +0300

    takaisin animaation palautus

--- projekti/src/routes/PelausSivu.svelte
+++ projekti/src/routes/PelausSivu.svelte
@@ -52,7 +52,7 @@
 			{ikoni}
 		></Kortti>
 	</div>
-	<div class="justify-center flex py-20">
+	<div class="justify-center flex py-20" in:fade={{ duration: 1000 }}>
 	<Button
 		tyyli={'bg-tekstit text-2xl font-josefin max-w-45 min-w-45 hover:shadow-xl/40 flex items-center justify-center max-h-15 min-h-15  rounded-xl py-2 pr-5 text-white cursor-pointer shadow-xl'}
 		text="Takaisin"
